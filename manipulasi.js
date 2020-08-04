515b8f9ff149b3a44d1426ded387a63b945e0ea4
heniardiana <heniardiana@gmail.com>
2020-08-04 07:27:06 +0700

    Project Dicoding
    mengerjain web portofolio

---
+++ manipulasi.js
@@ -0,0 +1,6 @@
+let imgToraja = document.querySelector("#imgToraja");
+imgToraja.setAttribute("src", "img/logo.png");
+
+
+const toraja = document.getElementById('toraja');
+toraja.innerHTML = 'Heni Ardiana';
